0a30c453042981a536cd17c6a563789827fae100
oskarziembrowicz <rakson2002@gmail.com>
2025-05-23 17:51:24 +0200

    Connected to database

--- controllers/partyController.js
+++ controllers/partyController.js
@@ -1,6 +1,13 @@
-exports.getAllParties = (req, res, next) => {
+const catchAsync = require('../utils/catchAsync');
+const Party = require('../models/partyModel');
+
+exports.getAllParties = catchAsync(async (req, res, next) => {
+  const parties = await Party.find();
+
   res.status(200).json({
     status: 'success',
-    data: {},
+    data: {
+      parties,
+    },
   });
-};
+});
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require("./app");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
+// Connect to database
+const DB = process.env.DATABASE.replace(
+  '<DB_PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose.set('strictQuery', false);
+mongoose.connect(DB).then(() => console.log('Connected to database'));
+
+// Start server
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
