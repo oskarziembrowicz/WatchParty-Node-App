e6c2890c1764783b203444e846ab8d9515382d27
Oskar Ziembrowicz <rakson2002@gmail.com>
2025-11-23 17:44:05 +0100

    Added login endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 const signToken = (id) =>
   // @ts-ignore - process.env values are checked at runtime
@@ -35,15 +36,38 @@ const createSendToken = (user, statusCode, res) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  console.log(req.body);
   const newUser = await User.create({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangedAt,
+    // passwordConfirm: req.body.passwordConfirm,
+    // passwordChangedAt: req.body.passwordChangedAt,
     role: req.body.role,
   });
 
   createSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1. Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  // 2. Check if the user exists and the password is correct
+  const user = await User.findOne({ email: email }).select('+password');
+
+  // TODO:
+  // if (!user || !(await user.correctPassword(password, user.password))) {
+  //   return next(new AppError("Incorrect email or password", 401));
+  // }
+
+  if (!user || user.password !== password) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // 3. Send token to client
+  createSendToken(user, 200, res);
 });
