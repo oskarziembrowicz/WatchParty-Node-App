d2e7698ffed5edc675f47ef2667e06720ba7d24a
Oskar Ziembrowicz <rakson2002@gmail.com>
2025-11-23 16:33:29 +0100

    Added user model

---
+++ dev-data/planning/plan-apki.md
@@ -0,0 +1,32 @@
+# Plan Aplikacji WatchParty Node
+
+## Backend
+
+- [x] Set up the project
+- [x] Connect to db
+- [x] Working first endpoint
+- [ ] Party endpoints:
+  - [ ] Create a party
+  - [x] Get a party
+  - [x] Get all parties
+  - [ ] Add movie
+  - [ ] Remove movie
+  - [ ] Add participant
+  - [ ] Remove participant
+  - [ ] Update party
+- [ ] User endpoints:
+  - [ ] Register
+  - [ ] Login
+  - [ ] Logout
+  - [ ] Get my data
+  - [ ] Get user by id
+  - [ ] Get all users
+  - [ ] Get parties
+  - [ ] Get hosted parties
+  - [ ] Add favorite movie
+  - [ ] Remove movie from favorites
+  - [ ] Add user as friend
+  - [ ] Remove user from friends
+- [ ] Movie endpoints:
+  - [ ] Find movie
+  - [ ] Get movie by id
---
+++ models/userModel.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'A user must have a username'],
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    minlength: [8, 'Password must be at least 8 characters'],
+    // select: false, // TODO: Don't return password by default
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+  },
+  savedMovies: {
+    type: [String],
+    default: [],
+  },
+  friends: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'User',
+    default: [],
+  },
+  parties: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Party',
+    default: [],
+  },
+  archivedParties: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Party',
+    default: [],
+  },
+}, {
+  timestamps: true, // Adds createdAt and updatedAt automatically
+});
+
+// TODO: Hash password before saving
+// userSchema.pre('save', async function(next) {
+//   // Only hash the password if it has been modified (or is new)
+//   if (!this.isModified('password')) return next();
+
+//   // Hash the password with cost of 12
+//   this.password = await bcrypt.hash(this.password, 12);
+//   next();
+// });
+
+// // Instance method to compare passwords
+// userSchema.methods.comparePassword = async function(candidatePassword) {
+//   return await bcrypt.compare(candidatePassword, this.password);
+// };
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
